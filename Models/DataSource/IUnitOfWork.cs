ecc9dd78f86c8c958a03e56a3e6d3335ac0ae913
mcseba <48526736+mcseba@users.noreply.github.com>
2021-03-17 15:48:03 +0100

    initial commit

---
+++ Models/DataSource/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.DataSource
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Commit();
+
+        void Rollback();
+    }
+}
